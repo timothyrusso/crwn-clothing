b3d7e550df4f40dc95d59ed1df4758838ae8795c
timothyrusso <russotimothy@live.it>
2022-10-16 13:05:38 +0200

    set up basic navigation bar with basic route structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <div>This is the shop page</div>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };

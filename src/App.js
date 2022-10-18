10471cf3e446965f250861a50b110a1d8800c92e
timothyrusso <russotimothy@live.it>
2022-10-18 20:21:08 +0200

    feat: new path for the sign in link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="signin" element={<SignIn />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );

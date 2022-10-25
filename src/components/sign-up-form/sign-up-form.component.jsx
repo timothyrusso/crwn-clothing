241f674331c6e31236e09de133e51dca46075297
Timothy Russo <85668106+timothyrusso@users.noreply.github.com>
2022-10-25 19:19:26 +0200

    feat: password and email user creation on the database method and reset form fields function

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { createAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -13,9 +13,31 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        createAuthUserWithEmailAndPassword(email, password);
+
+        if (password !== confirmPassword) {
+            alert('Password do not match')
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields(); 
+        }
+        catch (error) {
+            if (error.code == 'auth/mail-already-in-use') {
+                alert('Cannot create user, email already in use')
+            } else { console.log('User creation encountered an error', error); }
+        }
+
     }
 
     const handleChange = (event) => {
@@ -26,7 +48,7 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Sign up with your email and password</h1>
-            <form onSubmit={() => { }}>
+            <form onSubmit={handleSubmit}>
                 <label>Display Name</label>
                 <input type='text' required onChange={handleChange} name='displayName' value={displayName} />
                 <label>Email</label>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -34,7 +34,7 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 // Call the Firestore db
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
 
   // Check if the userAuth exist
   if (!userAuth) return;
@@ -56,6 +56,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message);

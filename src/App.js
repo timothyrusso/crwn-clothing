ef58783a9f453d59943f14231affe28168e19313
Timothy Russo <85668106+timothyrusso@users.noreply.github.com>
2022-11-08 19:11:44 +0100

    feat: shop component with mock data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return <div>This is the shop page</div>;
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (

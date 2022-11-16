34ab8f75304d3aa164c443f0fbc4fedc2989bcbb
timothyrusso <russotimothy@live.it>
2022-11-16 07:45:07 +0100

    feat: cart total price logic

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -48,19 +48,31 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   // Track the quantity of the items in the cart
   useEffect(() => {
-    const newCartCount = cartItems.reduce((total, cartItem) => {
-      return total + cartItem.quantity;
-    }, 0);
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
     setCartCount(newCartCount);
+  }, [cartItems]);
+
+  // Track the total amount of the price of the items in the cart
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
   }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
@@ -83,6 +95,7 @@ export const CartProvider = ({ children }) => {
     clearItemToCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,7 +4,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.styles.scss';
 
 const Checkout = () => {
-  const { cartItems } =
+  const { cartItems, cartTotal } =
     useContext(CartContext);
 
   return (
@@ -31,7 +31,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       )
       )}
-      <span className='total'>Total : 0</span>
+      <span className='total'>Total : ${cartTotal}</span>
     </div>
   );
 };

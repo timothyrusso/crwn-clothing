b594c0b6d55b72dc04e14a71cfead2a4a3598179
timothyrusso <russotimothy@live.it>
2022-11-20 17:02:10 +0100

    feat: setting up dynamic routes for the shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,12 @@
-import { useContext } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../../routes/categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return <CategoryPreview key={title} products={products} title={title} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 

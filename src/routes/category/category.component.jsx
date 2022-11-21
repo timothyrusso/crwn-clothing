203419c54b3446109f6e79cdf7230789888bd505
Timothy Russo <85668106+timothyrusso@users.noreply.github.com>
2022-11-21 19:34:34 +0100

    feat: link the categories in the category preview

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, UseEffect } from 'react'
+import { useContext, useState, UseEffect, Fragment } from 'react'
 import { useParams } from 'react-router-dom'
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component'
@@ -14,11 +14,14 @@ const Category = () => {
     }, [category, categoriesMap])
 
     return (
-        <div className='category-container'>
-            {
-                products && products.map((product) => <ProductCard key={product.id} product={product} />)
-            }
-        </div>
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {
+                    products && products.map((product) => <ProductCard key={product.id} product={product} />)
+                }
+            </div>
+        </Fragment>
     )
 }
 

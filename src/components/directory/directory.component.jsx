5cb1802c883e0ba72c6f794df7d65e13211147d9
Timothy Russo <85668106+timothyrusso@users.noreply.github.com>
2022-11-21 19:19:19 +0100

    fix: fix a component relative path for importing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import DirectoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ categories }) => {

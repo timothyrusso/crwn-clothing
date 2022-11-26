bdddb3df98aba68c83967cac3d90034d534789c6
timothyrusso <russotimothy@live.it>
2022-11-26 18:38:03 +0100

    feat: start to convert to styled components the form input component

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -1,14 +1,47 @@
-import styled, {css} from 'styled-component'
+import styled, { css } from 'styled-component';
 
-const subColor = 'grey'
-const mainColor = 'black'
+const subColor = 'grey';
+const mainColor = 'black';
 
-
-@mixin shrinkLabel {
+const shrinkLabelStyles = css`
   top: -14px;
   font-size: 12px;
-  color: $main-color;
-}
+  color: ${mainColor};
+`;
+
+export const FormInputLabel = styled.label`
+    color: ${subColor};
+    font-size: 16px;
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    left: 5px;
+    top: 10px;
+    transition: 300ms ease all;
+
+    ${({shrink}) => shrink && shrinkLabelStyles}
+`
+
+export const Input = styled.input`
+    background: none;
+    background-color: white;
+    color: ${subColor};
+    font-size: 18px;
+    padding: 10px 10px 10px 5px;
+    display: block;
+    width: 100%;
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid ${subColor};
+    margin: 25px 0;
+    &:focus {
+      outline: none;
+    }
+    &:focus ~ ${FormInputLabel} {
+      ${shrinkLabelStyles};
+    }
+`
+
 .group {
   position: relative;
   margin: 45px 0;
@@ -33,18 +66,5 @@ const mainColor = 'black'
   }
   input[type='password'] {
     letter-spacing: 0.3em;
-  }
-  .form-input-label {
-    color: $sub-color;
-    font-size: 16px;
-    font-weight: normal;
-    position: absolute;
-    pointer-events: none;
-    left: 5px;
-    top: 10px;
-    transition: 300ms ease all;
-    &.shrink {
-      @include shrinkLabel();
-    }
   }
 }

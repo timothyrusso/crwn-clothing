aadbe3df3bee863dc8f51dd8242c197545ee9caf
timothyrusso <russotimothy@live.it>
2022-11-26 10:37:59 +0100

    feat: new styled component container for cart dropdown

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from '../button/button.styles';
+
 export const CartDropdownContainer = styled.div`
   position: absolute;
   width: 240px;
@@ -13,7 +19,9 @@ export const CartDropdownContainer = styled.div`
   right: 40px;
   z-index: 5;
 
-  button {
+  ${BaseButton},
+  ${GoogleSignInButton},
+  ${InvertedButton} {
     margin-top: auto;
   }
 `;

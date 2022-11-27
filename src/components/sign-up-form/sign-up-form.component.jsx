4b2100fbd2125fd7e8fa97d5f6b1d21da81e56ff
timothyrusso <russotimothy@live.it>
2022-11-27 12:17:27 +0100

    feat: add a link to the main paige for all the categories

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -18,8 +18,6 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log('hit');
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };

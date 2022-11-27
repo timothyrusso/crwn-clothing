a7a9cc6c69289245ea050b734e1c184b5caf6eba
timothyrusso <russotimothy@live.it>
2022-11-27 11:50:59 +0100

    feat: complete the upgrade to styled components for the form input component

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-component';
+import styled, { css } from 'styled-components';
 
 const subColor = 'grey';
 const mainColor = 'black';
@@ -10,61 +10,43 @@ const shrinkLabelStyles = css`
 `;
 
 export const FormInputLabel = styled.label`
-    color: ${subColor};
-    font-size: 16px;
-    font-weight: normal;
-    position: absolute;
-    pointer-events: none;
-    left: 5px;
-    top: 10px;
-    transition: 300ms ease all;
+  color: ${subColor};
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
 
-    ${({shrink}) => shrink && shrinkLabelStyles}
-`
+  ${({ shrink }) => shrink && shrinkLabelStyles}
+`;
 
 export const Input = styled.input`
-    background: none;
-    background-color: white;
-    color: ${subColor};
-    font-size: 18px;
-    padding: 10px 10px 10px 5px;
-    display: block;
-    width: 100%;
-    border: none;
-    border-radius: 0;
-    border-bottom: 1px solid ${subColor};
-    margin: 25px 0;
-    &:focus {
-      outline: none;
-    }
-    &:focus ~ ${FormInputLabel} {
-      ${shrinkLabelStyles};
-    }
-`
+  background: none;
+  background-color: white;
+  color: ${subColor};
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid ${subColor};
+  margin: 25px 0;
+  &:focus {
+    outline: none;
+  }
+  &:focus ~ ${FormInputLabel} {
+    ${shrinkLabelStyles};
+  }
+`;
 
-.group {
+export const Group = styled.div`
   position: relative;
   margin: 45px 0;
-  .form-input {
-    background: none;
-    background-color: white;
-    color: $sub-color;
-    font-size: 18px;
-    padding: 10px 10px 10px 5px;
-    display: block;
-    width: 100%;
-    border: none;
-    border-radius: 0;
-    border-bottom: 1px solid $sub-color;
-    margin: 25px 0;
-    &:focus {
-      outline: none;
-    }
-    &:focus ~ .form-input-label {
-      @include shrinkLabel();
-    }
-  }
+
   input[type='password'] {
     letter-spacing: 0.3em;
   }
-}
+`;

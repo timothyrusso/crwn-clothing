ca48a05b7a23d514501f0921784f561bbfeb18e6
timothyrusso <russotimothy@live.it>
2022-11-28 19:07:16 +0100

    feat: turn all css files into styled components part three

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
-import './category.styles.scss';
+import { CategoryContainer, CategoryTitle } from './category.styles.jsx';
 
 const Category = () => {
   const { category } = useParams();
@@ -15,13 +15,13 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 };

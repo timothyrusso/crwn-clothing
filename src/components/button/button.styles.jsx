219d1fa6324d72a81a42352a35a774cd50f7e1ee
timothyrusso <russotimothy@live.it>
2022-11-29 18:33:28 +0100

    feat: remove letter spacing to improve the ui

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -4,7 +4,6 @@ export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
   height: 50px;
-  letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
   font-size: 15px;

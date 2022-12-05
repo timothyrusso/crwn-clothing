e600f6f69cc2af17210ad63870a10d53d9fb20f2
timothyrusso <russotimothy@live.it>
2022-12-05 07:34:07 +0100

    feat: start to set up the configuration for user in redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { useEffect } from 'react';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+import { setCurrentUser } from './store/user/user.action';
+
 import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
@@ -6,6 +13,20 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
+  // Centralized handler for user authentication.
+  // When the user state change, the listener method listen the event and update the user state.
+  // If the user doesn't exist,create a new one, if the user exist just login.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>

47e76fc06a5293890c79fbeeebbd6160b64b25b1
timothyrusso <russotimothy@live.it>
2022-12-21 07:51:15 +0100

    feat: test a new logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,26 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('next state: ', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
+// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+//   Boolean
+// );
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 

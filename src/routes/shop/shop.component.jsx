b0a8896ba7da4417c3b47cc888dd45aacfd44c0d
timothyrusso <russotimothy@live.it>
2022-12-27 17:17:09 +0100

    feat: migrate async logic from category component to redux thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,18 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import CategoriesPreview from '../../routes/categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import { useDispatch } from 'react-redux';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (

183a2a35e3b9db490fb9524d896e45ce516b605d
timothyrusso <russotimothy@live.it>
2022-12-27 16:12:33 +0100

    feat: add redux thunk and start the configuration for the category reducer

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { loggerMiddleware } from './middleware/logger';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
@@ -15,6 +16,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== 'production' && loggerMiddleware,
+  thunk,
 ].filter(Boolean);
 
 const composeEnhancer =

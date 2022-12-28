c8220f929b9bc0de012c1157a995afc793a75c15
timothyrusso <russotimothy@live.it>
2022-12-28 15:49:43 +0100

    feat: replace redux thunk with redux saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,12 +3,12 @@ import { useEffect } from 'react';
 import CategoriesPreview from '../../routes/categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { useDispatch } from 'react-redux';
-import { fetchCategoriesAsync } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
 
   return (

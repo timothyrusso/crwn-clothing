17d96c9e4136d1f491ae2f07788134a8b4987c5b
timothyrusso <russotimothy@live.it>
2023-01-17 07:28:15 +0100

    feat: use matchable types for the action creator methods

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,10 @@
-import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
-import { CategoryAction } from './category.action';
+import { AnyAction } from 'redux';
+import { Category } from './category.types';
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from './category.action';
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -15,16 +20,19 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
+  action = {} as AnyAction
 ) => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
 };

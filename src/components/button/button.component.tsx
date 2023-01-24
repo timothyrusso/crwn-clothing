279ffb05841f53a476c9d51693c0f4874d210c66
timothyrusso <russotimothy@live.it>
2023-01-24 19:33:20 +0100

    feat: migrate the button component to typescript

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -5,13 +5,15 @@ import {
   ButtonSpinner,
 } from './button.styles.jsx';
 
-export const BUTTON_TYPE_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted',
-};
+export enum BUTTON_TYPE_CLASSES {
+  base = 'base',
+  google = 'google-sign-in',
+  inverted = 'inverted',
+}
 
-export const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+export const getButton = (
+  buttonType = BUTTON_TYPE_CLASSES.base
+): typeof BaseButton =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,

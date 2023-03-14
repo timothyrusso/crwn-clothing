0cb94c7b794605039fe8b1b244ed0b7bda5fd2e8
Timothy Russo <russotimothy@live.it>
2023-03-14 19:03:36 +0100

    feat: add close checkout dropdown functionality

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,21 +1,30 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../store/cart/cart.selector';
+import { selectCartItems, selectIsCartOpen } from '../../store/cart/cart.selector';
 import {
   CartDropdownContainer,
   CartItems,
   EmptyMessage,
 } from './cart-dropdown.styles';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useCallback } from 'react';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 const CartDropdown = () => {
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+
   const goToCheckoutHandler = useCallback(() => {
     navigate('/checkout');
+    toggleIsCartOpen();
   }, []);
+
 
   const cartItems = useSelector(selectCartItems);
 
